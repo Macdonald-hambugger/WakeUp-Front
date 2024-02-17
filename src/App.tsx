6b6237dd3486c25b433e8846cdffa07c60d2e3e9
Ubinquitous <ubinquitous1@gmail.com>
2024-02-17 19:16:47 +0900

    test : login

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import Login from 'pages/Login';
 import Main from 'pages/Main';
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import OAuth from 'pages/OAuth';
 
 const App = () => {
   return (
@@ -13,6 +14,7 @@ const App = () => {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/main" element={<Main />} />
+        <Route path="/oauth" element={<OAuth />} />
         <Route path="/alarm">
           <Route path="add" element={<AlarmAdd />} />
           <Route path="detail" element={<AlarmDetail />} />
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,5 +1,12 @@
+const url =
+  'https://kauth.kakao.com/oauth/authorize?client_id=a70ee38977223dbfad44a14a6f3b2922&redirect_uri=http://localhost:3000/oauth&response_type=code';
+
 const Login = () => {
-  return <>Login</>;
+  return (
+    <div>
+      <a href={url}>logi</a>
+    </div>
+  );
 };
 
 export default Login;
